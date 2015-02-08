26526ea71f5191d2b97f195506c6cef932008a97
COConsulting <info@newconsultant.us>
2015-02-07 17:21:20 -0700

    Assignment - 1st Commit
    This is the 3rd Challenge for my iOS8 and Swift online course.
    
    From the instructor:
    
    Objective:
    Our goal for this challenge, will be to have the user enter a number in
    the textfield, convert it to dog years, and lastly display that new
    value in the label.
    
    The key here is to break up the problem into steps. This will help you
    when solving this entry level challenge, as well as future bigger
    challenges. Feel free to use our recommended steps, or create your own.
    
    Recommended Steps:
    - Create a New Xcode Project.
    - Implement our Image Assets
    - Setup Storyboard Elements
    — ImageView
    — Textfield
    —- Be sure to check which keyboard style you are using with this. We
    are only dealing with numbers, so find one that fits that need.
    — Label
    — Button
    - Hook these storyboard elements up with our view controller.
    - Figure out how to grab the entered string value from the textfield.
    - Convert string variable to an int variable. (*Hint* try using an
    optional, but make sure to unwrap it!)
    - Create a new variable and set that value to our entered value int * 7
    (since the total dog years is equal to human years multiplied by 7).
    - Then you want to get that new int and convert it to a string
    - Lastly, display this newly converted string to your label.

--- Assignment-AgeOfLaikaApp/ViewController.swift
+++ Assignment-AgeOfLaikaApp/ViewController.swift
@@ -10,6 +10,11 @@ import UIKit
 
 class ViewController: UIViewController {
 
+//  Activating Outlets
+    @IBOutlet weak var enterDogAgeHumanYrs: UITextField!
+    @IBOutlet weak var convertedDogAge: UILabel!
+    
+//  System set functions
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -20,6 +25,25 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+//  Activating Button Actions
+    @IBAction func convertToDogYrs(sender: UIButton) {
 
+//      Hide keyboard
+        enterDogAgeHumanYrs.resignFirstResponder()
+        
+//      Setup a new variable to equal the inputed human years and multiply that by 7
+        var dogAgeDogYrs = (enterDogAgeHumanYrs.text.toInt()! * 7)
+        
+//      Unhide the Converted Age Label
+        convertedDogAge.hidden=false
+        
+//      Change Converted Age Label to being the calculated Age
+        convertedDogAge.text = "Your dog is \(dogAgeDogYrs) in Dog years."
+        
+//      Delete inputed text 
+        enterDogAgeHumanYrs.text=""
+        
+    }
+    
 }
 
