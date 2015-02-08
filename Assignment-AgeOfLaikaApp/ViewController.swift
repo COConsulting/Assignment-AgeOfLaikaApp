b32bea94d63cb5b5f2181d95df380180e8ac7b31
COConsulting <info@newconsultant.us>
2015-02-07 18:03:40 -0700

    Extra Work
    I added a bit more functionality to this app.
    
    - When the convert button is pressed, it and the input field disappear
    and are replaced by the answer and a convert again button.
    - When the convert again button is pushed, it and the answer are hidden
    and the initial input field and button return.
    - There is a fatal crash that occurs in the original program if someone
    pushes the convert button and there is nothing entered into the field.
    I added a conditional that checks for this and if nothing is inputed it
    reports and error to the user and asks for input.
    - I also added the ability for the person to enter their dog’s name.

--- Assignment-AgeOfLaikaApp/ViewController.swift
+++ Assignment-AgeOfLaikaApp/ViewController.swift
@@ -11,8 +11,14 @@ import UIKit
 class ViewController: UIViewController {
 
 //  Activating Outlets
+    @IBOutlet weak var enterDogName: UITextField!
     @IBOutlet weak var enterDogAgeHumanYrs: UITextField!
     @IBOutlet weak var convertedDogAge: UILabel!
+    @IBOutlet weak var convertToDogYrsButton: UIButton!
+    @IBOutlet weak var convertAgainButton: UIButton!
+    @IBOutlet weak var errorMessageLabel: UILabel!
+    @IBOutlet weak var errorNameLabel: UILabel!
+    @IBOutlet weak var errorMessage: UILabel!
     
 //  System set functions
     override func viewDidLoad() {
@@ -27,23 +33,70 @@ class ViewController: UIViewController {
 
 //  Activating Button Actions
     @IBAction func convertToDogYrs(sender: UIButton) {
+        
+//      Check for input, if none, report error, otherwise continue.
+        if(enterDogAgeHumanYrs.text=="" || enterDogName.text==""){
+            
+//          Reset all error messages incase a person makes an error, fixes one but not the other.
+            errorNameLabel.hidden=true
+            errorMessageLabel.hidden=true
+            
+            
+//          Unhide error message and error corresponding label near correct input box(es)
+            
+            errorMessage.hidden=false
+            
+            if(enterDogName.text==""){
+                errorNameLabel.hidden=false
+            }
+            if(enterDogAgeHumanYrs.text==""){
+                errorMessageLabel.hidden=false
+            }
+            
+        }else{
+            
+//          Make sure that the error message and label are hidden.
+            errorMessageLabel.hidden=true
+            errorNameLabel.hidden=true
+            errorMessage.hidden=true
+        
+//          Setup a new variable to equal the inputed human years and multiply that by 7
+            var dogAgeDogYrs = (enterDogAgeHumanYrs.text.toInt()! * 7)
+        
+//          Change Converted Age Label to being the calculated Age
+            convertedDogAge.text = "Your dog, "+enterDogName.text+", is \(dogAgeDogYrs) in Dog years."
 
-//      Hide keyboard
-        enterDogAgeHumanYrs.resignFirstResponder()
+//          Hide Input boxs and button
+            enterDogAgeHumanYrs.hidden=true
+            enterDogName.hidden=true
+            convertToDogYrsButton.hidden=true
         
-//      Setup a new variable to equal the inputed human years and multiply that by 7
-        var dogAgeDogYrs = (enterDogAgeHumanYrs.text.toInt()! * 7)
-        
-//      Unhide the Converted Age Label
-        convertedDogAge.hidden=false
-        
-//      Change Converted Age Label to being the calculated Age
-        convertedDogAge.text = "Your dog is \(dogAgeDogYrs) in Dog years."
-        
-//      Delete inputed text 
-        enterDogAgeHumanYrs.text=""
-        
+//          Show the result by unhiding the Converted Age Label and the convert again button
+            convertedDogAge.hidden=false
+            convertAgainButton.hidden=false
+
+//          CLEAN UP THE UI ELEMENTS
+//          Hide keyboard
+            enterDogAgeHumanYrs.resignFirstResponder()
+            
+        }
     }
     
+    @IBAction func convertAgainButton(sender: UIButton) {
+
+//      Delete previously inputed text
+        enterDogName.text=""
+        enterDogAgeHumanYrs.text=""
+        
+//      Hide Converted Age and Convert Again Button
+        convertedDogAge.hidden=true
+        convertAgainButton.hidden=true
+        
+//      Unhide input box and inital Convert Button
+        enterDogName.hidden=false
+        enterDogAgeHumanYrs.hidden=false
+        convertToDogYrsButton.hidden=false
+        
+    }
 }
 
