701a8f84e9fe1b1036205d1a48bb6ec3ecbb7edf
COConsulting <info@newconsultant.us>
2015-02-12 22:02:07 -0700

    Assignment 2 - Add "Real" Dog Years
    In this commit, I do the second assignment for the Age of Laika App for
    my iOS8 and Swift class where I need to convert the age given into
    “real” dog years; which is 10.5 years for the first 2 years and then 4
    years for every year after.
    
    I did this, plus left the initial button for “regular” dog years. The
    app still checks for if a name was given for both buttons and displays
    a name or “Your dog” if no name given.
    
    Also, I added in the check to see if someone entered “0” as an age, and
    if they did, then the app tells them that the dog has not been born
    yet.
    
    As this app is now working, I will call it done. But, I could see a few
    next steps for added functionality.
    - Add a button after a conversion is made for the user to convert the
    other age. So, if they push for “Dog Years” then a button to convert
    into “Real” dog years would show up with the answer (and vise versa).
    - I could also see adding a button on the initial screen to show both
    conversions at once.
    - I am also sure that there could be some cleaning up of my code to
    make it smoother.
    - And I could see adding a main screen where the user could select the
    type of animal to find the age, and then duplicate the code here to do
    an age for a cat, horse, fish, bunny, etc.
    
    But, for now, I will call it done.

---
+++ Assignment-AgeOfLaikaApp/Double Tests.playground/section-1.swift
@@ -0,0 +1,9 @@
+// Playground - noun: a place where people can play
+
+import UIKit
+
+var str = "Hello, playground"
+
+var doubleString = "3.9585"
+var doubleValueFromString = Double((doubleString as NSString).doubleValue)
+doubleValueFromString += 3.85
--- Assignment-AgeOfLaikaApp/ViewController.swift
+++ Assignment-AgeOfLaikaApp/ViewController.swift
@@ -15,6 +15,7 @@ class ViewController: UIViewController {
     @IBOutlet weak var enterDogAgeHumanYrs: UITextField!
     @IBOutlet weak var convertedDogAge: UILabel!
     @IBOutlet weak var convertToDogYrsButton: UIButton!
+    @IBOutlet weak var convertToRealDogYrsButton: UIButton!
     @IBOutlet weak var convertAgainButton: UIButton!
     @IBOutlet weak var errorMessageLabel: UILabel!
     @IBOutlet weak var errorMessage: UILabel!
@@ -33,6 +34,8 @@ class ViewController: UIViewController {
 //  Activating Button Actions
     @IBAction func convertToDogYrs(sender: UIButton) {
         
+        var convertMessage = "Need a Message?"
+        
 //      Check for input, if none, report error, otherwise continue.
         if(enterDogAgeHumanYrs.text==""){
             
@@ -46,27 +49,135 @@ class ViewController: UIViewController {
             errorMessageLabel.hidden=true
             errorMessage.hidden=true
         
-//          Setup a new variable to equal the inputed human years and multiply that by 7
-            var dogAgeDogYrs = (enterDogAgeHumanYrs.text.toInt()! * 7)
-        
-//          Check to see if name is inputed
-            if(enterDogName.text==""){
-//              Change Converted Age Label to being the calculated Age
-                convertedDogAge.text = "Your dog is \(dogAgeDogYrs) in dog years."
-            }else{
-//              Change Converted Age Label to being the calculated Age
-                convertedDogAge.text = enterDogName.text + " is \(dogAgeDogYrs) in dog years."
-            }
+            var dogAgeDogYrsString = enterDogAgeHumanYrs.text
+            var dogAgeDogYrsDouble = Double((dogAgeDogYrsString as NSString).doubleValue)
 
-//          Hide Input boxes and button
+            if (dogAgeDogYrsDouble == 0.0){
+                
+//              Say that the dog hasn't been born yet.
+//              Check for Dog name
+                if (enterDogName.text == ""){
+                    convertMessage = "Your dog hasn't been born yet."
+                }else{
+                    convertMessage = enterDogName.text + " hasn't been born yet."
+                }
+                
+            }else{
+//              Setup a new variable to equal the inputed human years and multiply that by 7
+                dogAgeDogYrsDouble *= 7
+            }
+            
+            if (convertMessage == "Need a Message?"){
+
+//              Check to see if name is inputed
+                if(enterDogName.text==""){
+//                  Change Converted Age Label to being the calculated Age
+                    convertMessage = "Your dog is \(dogAgeDogYrsDouble) in dog years."
+                }else{
+//                  Change Converted Age Label to being the calculated Age
+                    convertMessage = enterDogName.text + " is \(dogAgeDogYrsDouble) in dog years."
+                }
+            }
+            
+//          Store converted Text to be displayed
+            convertedDogAge.text = convertMessage
+
+
+//          Hide Input boxes and buttons
             enterDogAgeHumanYrs.hidden=true
             enterDogName.hidden=true
             convertToDogYrsButton.hidden=true
+            convertToRealDogYrsButton.hidden=true
         
 //          Show the result by unhiding the Converted Age Label and the convert again button
             convertedDogAge.hidden=false
             convertAgainButton.hidden=false
 
+//          CLEAN UP THE UI ELEMENTS
+//          Hide keyboards
+            enterDogAgeHumanYrs.resignFirstResponder()
+            enterDogName.resignFirstResponder()
+        }
+        
+    }
+    
+    @IBAction func convertToRealDogYrsButtonPressed(sender: UIButton) {
+        
+        var convertMessage = "Need a Message?"
+        
+//      Check for input, if none, report error, otherwise continue.
+        if(enterDogAgeHumanYrs.text==""){
+
+//          Set errorMessage
+            errorMessage.text = "Please enter an age above."
+                
+//          Unhide error message and error label near input box
+            errorMessage.hidden=false
+            errorMessageLabel.hidden=false
+            
+        }else{
+            
+//          Make sure that the error message and label are hidden.
+            errorMessageLabel.hidden=true
+            errorMessage.hidden=true
+            
+//          Setup a new variable to equal the inputed human years
+            var dogAgeDogYrsString = enterDogAgeHumanYrs.text
+            var dogAgeDogYrsDouble = Double((dogAgeDogYrsString as NSString).doubleValue)
+            
+//          Multiply the first 2 years by 10.5 and the following years by 4
+            if (dogAgeDogYrsDouble == 0.0){
+
+//              Say that the dog hasn't been born yet.
+//              Check for Dog name
+                if (enterDogName.text == ""){
+                    convertMessage = "Your dog hasn't been born yet."
+                }else{
+                    convertMessage = enterDogName.text + " hasn't been born yet."
+                }
+                
+            }else if(dogAgeDogYrsDouble > 0 && dogAgeDogYrsDouble <= 2){ //Then check to see if the age is between 0 and 2
+
+//  To simplify the code I could have done else if (dogAgeDogYrsDouble > 2) and then flipped what was in this else if statement with what is in the else statement next. But, I choose to keep this way to have a chronological flow of looking for an age of 0, then between 0 and 2 and then above 2.
+                
+                dogAgeDogYrsDouble *= 10.5
+                
+            }else{ //Then the age must be over 2
+//              Figure out how many years over 2 the dog is. (do 2.0 to make agePart1 a Double)
+                var agePart1 = 2.0
+                var agePart2 = dogAgeDogYrsDouble - agePart1
+                
+//              multiply the first 2 years by 10.5 and the remainer of the age by 4
+                dogAgeDogYrsDouble = (agePart1*10.5) + (agePart2*4)
+                
+            }
+            
+//          Check to see if a message is already made
+            if (convertMessage == "Need a Message?"){
+                
+//              Check to see if name is inputed
+                if(enterDogName.text==""){
+//                  Change Converted Age Label to being the calculated Age
+                    convertMessage = "Your dog is \(dogAgeDogYrsDouble) in real dog years."
+                }else{
+//                  Change Converted Age Label to being the calculated Age
+                    convertMessage = enterDogName.text + " is \(dogAgeDogYrsDouble) in real dog years."
+                }
+            }
+            
+//          Store converted Text to be displayed
+            convertedDogAge.text = convertMessage
+            
+//          Hide Input boxes and buttons
+            enterDogAgeHumanYrs.hidden=true
+            enterDogName.hidden=true
+            convertToDogYrsButton.hidden=true
+            convertToRealDogYrsButton.hidden=true
+            
+//          Show the result by unhiding the Converted Age Label and the convert again button
+            convertedDogAge.hidden=false
+            convertAgainButton.hidden=false
+            
 //          CLEAN UP THE UI ELEMENTS
 //          Hide keyboards
             enterDogAgeHumanYrs.resignFirstResponder()
@@ -88,6 +199,7 @@ class ViewController: UIViewController {
         enterDogName.hidden=false
         enterDogAgeHumanYrs.hidden=false
         convertToDogYrsButton.hidden=false
+        convertToRealDogYrsButton.hidden=false
         
     }
 }
